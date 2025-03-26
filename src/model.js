38ff1bf47f48a269aeeb17e11d5f49c163aa1fe3
PHNTM02 <147247828+PHNTM02@users.noreply.github.com>
2025-03-26 11:12:52 +0800

    Update model.js

--- src/model.js
+++ src/model.js
@@ -1,17 +1,17 @@
 export const models = [
     {
         name: "pioneer",
-        path: "./models/PIOHNEERNew.gltf",
+        path: "../models/PIOHNEERNew.gltf",
         scale: 2
     },
     {
         name: "cah",
-        path: "./models/CAH.gltf",
+        path: "../models/CAH.gltf",
         scale: 3
     },
     {
         name: "tree1",
-        path: "./models/Tree1.gltf",
+        path: "../models/Tree1.gltf",
         scale: 1
     },
 ];

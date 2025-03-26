b7081076d53491eefe41643086fa0c467cf02e6b
PHNTM02 <147247828+PHNTM02@users.noreply.github.com>
2025-03-26 10:46:20 +0800

    Yey

--- src/assets.js
+++ src/assets.js
@@ -1,70 +1,116 @@
+// import * as THREE from 'three';
+// import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+// export class GetModel extends THREE.Group {
+//     constructor(tiles, tileSize = 1) {
+//         super();
+//         this.loader = new GLTFLoader();
+//         this.tiles = tiles;
+//         this.tileSize = tileSize;
+
+//         // Define model positions (supports .gltf & .glb)
+//         this.modelPositions = {
+//             'models/PIONEERNew.gltf': { x: 2, z: 2 },
+//             'models/CAH.gltf': { x: -3, z: 1 },
+//             'models/Tree1.gltf': { x: 0, z: -2 } // Example GLB model
+//         };
+
+//         this.loadModels();
+//     }
+
+//     loadModels() {
+//         for (const modelPath in this.modelPositions) {
+//             this.loadModel(modelPath, this.modelPositions[modelPath]);
+//         }
+//     }
+
+//     loadModel(modelPath, tileCoords) {
+//         this.loader.load(
+//             modelPath,
+//             (gltf) => {
+//                 const model = gltf.scene;
+//                 model.scale.set(0.08, 0.08, 0.08);
+//                 model.traverse((child) => {
+//                     if (child.isMesh) {
+//                         child.castShadow = true;
+//                         child.receiveShadow = true;
+//                     }
+//                 });
+
+//                 this.placeModelOnTile(model, tileCoords);
+//                 this.add(model);
+//             },
+//             undefined,
+//             (error) => {
+//                 console.error(`Error loading model ${modelPath}:`, error);
+//             }
+//         );
+//     }
+
+//     placeModelOnTile(model, tileCoords) {
+//         let targetTile = this.findTileByCoords(tileCoords);
+//         if (targetTile) {
+//             model.position.set(
+//                 targetTile.position.x,  
+//                 targetTile.position.y + 0.2,  
+//                 targetTile.position.z   
+//             );
+//             targetTile.material.color.set(0xff0000); // Highlight the tile
+//         } else {
+//             console.warn(`No tile found at (${tileCoords.x}, ${tileCoords.z}) for model placement.`);
+//         }
+//     }
+
+//     findTileByCoords(coords) {
+//         return this.tiles.find(tile => 
+//             Math.round(tile.position.x) === coords.x &&
+//             Math.round(tile.position.z) === coords.z
+//         );
+//     }
+// }
+
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { models } from './model.js';
 
-export class GetModel extends THREE.Group {
-    constructor(tiles, tileSize = 1) {
+export class AssetManager extends THREE.Group {
+    constructor(tileSize = 1) {
         super();
         this.loader = new GLTFLoader();
-        this.tiles = tiles;
-        this.tileSize = tileSize;
+        this.tileSize = tileSize; // Tile size for reference
 
-        // Define model positions (supports .gltf & .glb)
-        this.modelPositions = {
-            'models/PIONEERNew.gltf': { x: 2, z: 2 },
-            'models/CAH.gltf': { x: -3, z: 1 },
-            'models/Tree1.gltf': { x: 0, z: -2 } // Example GLB model
+        // Define positions for each model
+        this.modelData = {
+            'pioneer': { x: 2, z: 2 },
+            'cah': { x: -3, z: 1 },
+            'tree1': { x: 0, z: -2 }
         };
 
         this.loadModels();
     }
 
+    // Function to calculate scale based on tile size
+    modelSize(modelKey, baseScale) {
+        return (baseScale / 3) * this.tileSize;
+    }
+
     loadModels() {
-        for (const modelPath in this.modelPositions) {
-            this.loadModel(modelPath, this.modelPositions[modelPath]);
-        }
-    }
+        models.forEach((model) => {
+            const modelKey = model.path.split('/').pop().replace('.gltf', '');
+            const modelInfo = this.modelData[modelKey];
 
-    loadModel(modelPath, tileCoords) {
-        this.loader.load(
-            modelPath,
-            (gltf) => {
-                const model = gltf.scene;
-                model.scale.set(0.08, 0.08, 0.08);
-                model.traverse((child) => {
-                    if (child.isMesh) {
-                        child.castShadow = true;
-                        child.receiveShadow = true;
-                    }
+            if (modelInfo) {
+                this.loader.load(model.path, (gltf) => {
+                    const object = gltf.scene;
+                    const adjustedScale = this.modelSize(modelKey, model.scale);
+
+                    object.scale.set(adjustedScale, adjustedScale, adjustedScale);
+                    object.position.set(modelInfo.x, 0, modelInfo.z);
+                    this.add(object);
                 });
-
-                this.placeModelOnTile(model, tileCoords);
-                this.add(model);
-            },
-            undefined,
-            (error) => {
-                console.error(`Error loading model ${modelPath}:`, error);
+            } else {
+                console.warn(`No position data defined for ${model.path}`);
             }
-        );
-    }
-
-    placeModelOnTile(model, tileCoords) {
-        let targetTile = this.findTileByCoords(tileCoords);
-        if (targetTile) {
-            model.position.set(
-                targetTile.position.x,  
-                targetTile.position.y + 0.2,  
-                targetTile.position.z   
-            );
-            targetTile.material.color.set(0xff0000); // Highlight the tile
-        } else {
-            console.warn(`No tile found at (${tileCoords.x}, ${tileCoords.z}) for model placement.`);
-        }
-    }
-
-    findTileByCoords(coords) {
-        return this.tiles.find(tile => 
-            Math.round(tile.position.x) === coords.x &&
-            Math.round(tile.position.z) === coords.z
-        );
+        });
     }
 }
---
+++ src/model.js
@@ -0,0 +1,17 @@
+export const models = [
+    {
+        name: "pioneer",
+        path: "/models/PIOHNEERNew.gltf",
+        scale: 2
+    },
+    {
+        name: "cah",
+        path: "/models/CAH.gltf",
+        scale: 3
+    },
+    {
+        name: "tree1",
+        path: "/models/Tree1.gltf",
+        scale: 1
+    },
+];

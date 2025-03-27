622905f3427c2330d30f4408d169b85028854227
PHNTM02 <147247828+PHNTM02@users.noreply.github.com>
2025-03-27 12:21:07 +0800

    Update terrain.js

--- src/terrain.js
+++ src/terrain.js
@@ -14,31 +14,35 @@ export class Terrain extends THREE.Group {
     }
 
     terrain() {
-        const tileSize = 1;
-        const gridSize = 100; 
+        const tileSize = 100;
+        // const gridSize = 100; 
         
         // Create a box geometry for the tiles
         const geometry = new THREE.BoxGeometry(tileSize, 0.3, tileSize);
         const material = new THREE.MeshBasicMaterial({ color: 0x76e895 });
+        const terrain = new THREE.Mesh( geometry, material );
+        this.add(terrain);
 
-        // Number of instances (100x100)
-        const count = gridSize * gridSize;
 
-        // Create an InstancedMesh (100x100 instances of the box)
-        const instancedMesh = new THREE.InstancedMesh(geometry, material, count);
 
-        // Set the positions for each tile in the grid
-        const matrix = new THREE.Matrix4();
-        for (let x = 0; x < gridSize; x++) {
-            for (let z = 0; z < gridSize; z++) {
-                const posX = x * tileSize;
-                const posZ = z * tileSize;
+        // // Number of instances (100x100)
+        // const count = gridSize * gridSize;
 
-                matrix.setPosition(posX, 0, posZ);
-                instancedMesh.setMatrixAt(x * gridSize + z, matrix);
-            }
-        }
-        this.add(instancedMesh);
+        // // Create an InstancedMesh (100x100 instances of the box)
+        // const instancedMesh = new THREE.InstancedMesh(geometry, material, count);
+
+        // // Set the positions for each tile in the grid
+        // const matrix = new THREE.Matrix4();
+        // for (let x = 0; x < gridSize; x++) {
+        //     for (let z = 0; z < gridSize; z++) {
+        //         const posX = x * tileSize;
+        //         const posZ = z * tileSize;
+
+        //         matrix.setPosition(posX, 0, posZ);
+        //         instancedMesh.setMatrixAt(x * gridSize + z, matrix);
+        //     }
+        // }
+        // this.add(instancedMesh);
     }
 
     loadAssets() {

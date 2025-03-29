15f5cb5c252abe887077b134ea5decd53d804d0f
lyzel27 <93996081+lyzel27@users.noreply.github.com>
2025-03-30 02:41:04 +0800

    new

--- src/model.js
+++ src/model.js
@@ -1,7 +1,7 @@
 export const models = [
     {
         name: "pioneer",
-        path: "./models/Model/PIONEERNew.gltf",
+        path: "./models/Model/PIONEER.gltf",
         scale: 2
     },
     
@@ -10,34 +10,51 @@ export const models = [
         path: "./models/Model/CAH.gltf",
         scale: 3
     },
+
     {
         name: "tree1",
         path: "./models/Model/Tree1.gltf",
         scale: 1
     },
+
     {
         name: "cod",
-        path: "./models/Model/CODNew.gltf",
+        path: "./models/Model/COD.gltf",
         scale: 4
     },
+
     {
         name: "com",
-        path: "./models/Model/COMNew.gltf",
+        path: "./models/Model/COM.gltf",
         scale: 5
     },
+
     {
         name: "cst",
-        path: "./models/Model/CSTNew.gltf",
+        path: "./models/Model/CST.gltf",
         scale: 6,
     },
+
     {
         name: "aupgate",
         path: "./models/Model/AUP Gate.gltf",
         scale: 7
     },
+
     {
         name: "CAHcoeCOT",
         path: "./models/CaHCotCoE.gltf",
         scale: 8
+    },
+    
+    {
+        name: "COE",
+        path: "./models/COE.gltf",
+        scale: 9
+    },
+
+    {
+        name: "COT",
+        path: "./models/COT.gltf",
     },
 ];
